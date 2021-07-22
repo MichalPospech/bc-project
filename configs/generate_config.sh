9d8623e65563ce2274a015d160b058dea945f878
Michal Pospěch <michal.pospech@gmail.com>
2021-07-22 23:50:42 +0200

    submission

--- configs/generate_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SEEDS=${@:2}
-CONFIG_NAME=$1
-
-for SEED in $SEEDS; do
-    cp $CONFIG_NAME.json.template $CONFIG_NAME.$SEED.json
-    sed -i "s/SEED/$SEED/g" $CONFIG_NAME.$SEED.json
-done
